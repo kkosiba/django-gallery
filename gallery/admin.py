598f2dd57828a9c022282bb04d4b4172d504247b
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-08-08 15:27:03 +0100

    Registered models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Category
+from .models import Picture
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Picture)

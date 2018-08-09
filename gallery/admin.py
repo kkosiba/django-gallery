d49b62c5d8fd9f3240eb6d7d33cc08dc8814005c
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-08-09 23:32:40 +0100

    Registered user profile to admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import Category
 from .models import Picture
+from .models import Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Picture)
+admin.site.register(Profile)

608dcd841f567d44344bb479918961417922f832
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-08-12 16:32:34 +0100

    Reordered apps to use custom registration templates

--- website/settings.py
+++ website/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery',
 ]
 
 MIDDLEWARE = [

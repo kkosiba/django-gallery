c853e2d7ec7e7eb0f65baccad2365e6c3d8167ad
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-09-05 13:31:58 +0100

    Added multiple image upload functionality
    Other changes:
    1. Added views to display current albums, group pictures by album, etc.
    2. Replaced Categories with Albums

--- gallery/views.py
+++ gallery/views.py
@@ -3,16 +3,14 @@ from django.utils import timezone
 from django.http import Http404
 
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+# from django.utils.decorators import method_decorator
 
 from django.db import transaction
 
 from django.contrib.auth.models import User
-from .models import Profile
-from .forms import CreatePictureForm
+from .models import Profile, Album, Picture
+from .forms import CreatePictureForm, AlbumCreateForm
 from .forms import UserForm, ProfileForm, SignUpForm
-
-from .models import Category, Picture
 
 from django.urls import reverse_lazy
 
@@ -32,14 +30,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Q
 
 from django.contrib import messages
-
 # Create your views here.
 
 class Index(ListView):
     model = Picture
     template_name = 'gallery/index.html'
     context_object_name = 'pictures'
-    paginate_by = 50
+    paginate_by = 10
     ordering = ('-published_date', )
     
 
@@ -47,6 +44,11 @@ class SignUp(CreateView):
     template_name = 'registration/signup.html'
     form_class = SignUpForm
     success_url = reverse_lazy('login')
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('/')
+        return super().dispatch(*args, **kwargs)
 
 
 class About(TemplateView):
@@ -56,6 +58,38 @@ class About(TemplateView):
 class PictureCreate(LoginRequiredMixin, CreateView):
     model = Picture
     form_class = CreatePictureForm
+
+    # to process request.user and all files in the form
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        form.instance.author = self.request.user
+        files = self.request.FILES.getlist('picture')
+        if files:
+            for file in files:
+                obj = self.model.objects.create(
+                    title=form.cleaned_data['title'],
+                    picture=file,
+                    author=form.instance.author,
+                    description=form.cleaned_data['description'])
+        return super().form_valid(form)
+
+
+class AlbumCreate(LoginRequiredMixin, CreateView):
+    model = Album
+    form_class = AlbumCreateForm
+
+    # to process request user in the form
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class AlbumDelete(DeleteView):
+    pass
+
+
+class AlbumUpdate(UpdateView):
+    pass
 
 
 class PictureSearch(ListView):
@@ -70,7 +104,7 @@ class PictureSearch(ListView):
         results = []
         if search_query:
             results = Picture.objects.filter(
-                Q(category__name__icontains=search_query) |
+                Q(album__name__icontains=search_query) |
                 Q(author__first_name__icontains=search_query) |
                 Q(title__icontains=search_query) |
                 Q(description__icontains=search_query)).distinct()
@@ -92,9 +126,42 @@ class ListPicturesByAuthor(ListView):
         return results
 
 
-class ListCategories(ListView):
-    pass
+class ListAlbums(ListView):
+    model = Album
+    template_name = 'gallery/list_albums.html'
+    context_object_name = 'albums'
+    paginate_by = 10
 
+
+class ListPicturesByAlbum(ListView):
+    model = Picture
+    context_object_name = 'pictures'
+    template_name = 'gallery/pictures_by_album.html'
+    paginate_by = 50
+    ordering = ('-published_date',)
+
+    def get_queryset(self):
+        album = self.kwargs.get('album_name', None)
+        results = []
+        if album:
+            results = Picture.objects.filter(album__name=album)
+        return results
+
+
+class ListPicturesByTags(ListView):
+    model = Picture
+    context_object_name = 'pictures'
+    template_name = 'gallery/pictures_by_tags.html'
+    paginate_by = 50
+    ordering = ('-published_date',)
+
+    def get_queryset(self):
+        tag = self.kwargs.get('tag_name', None)
+        results = []
+        if tag:
+            results = Picture.objects.filter(
+                tags__name=tag)
+        return results
 
 class PictureDetails(DetailView):
     model = Picture

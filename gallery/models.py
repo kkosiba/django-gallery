bf713c47a7cf54b57383aa47a04ac8024da90e28
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-09-20 09:57:38 +0100

    Removed irrelevant fields

--- gallery/models.py
+++ gallery/models.py
@@ -3,10 +3,6 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 # tags
 from taggit.managers import TaggableManager
 
@@ -43,21 +39,17 @@ class Album(models.Model):
 
 
 class Picture(models.Model):
-    album = models.ManyToManyField(Album, blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pictures')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='pictures')
     picture = models.ImageField(upload_to='media', default='media/None/no-img.jpg')
-    title = models.CharField(max_length=300)
-    published_date = models.DateTimeField(default=timezone.now)
     description = models.CharField(max_length=500, default='Empty')
 
     # tags mechanism
-    tags = TaggableManager()
-    
-    class Meta:
-        ordering = ('-published_date', )
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('gallery:single_picture', kwargs={'pk': self.pk})
+        return reverse('gallery:single_picture', 
+            kwargs={'album_name': self.album.name,
+                    'pk': self.pk})

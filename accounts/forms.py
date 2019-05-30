e85f50318d6ea738cc55e633e6adac494ecf6fb2
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-05-30 10:00:34 +0100

    Cleaned code in accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,50 +1,39 @@
 from django import forms
-
-from django.forms import (
-    ModelForm, TextInput, EmailField,
-    EmailInput, CharField, PasswordInput,
-    )
-
+from django.contrib.auth import forms as auth_forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
 from django.core.exceptions import ValidationError
 
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', )
+        fields = ("first_name", "last_name", "email")
 
 
-class SignUpForm(UserCreationForm):
-    username = CharField(label='Username',
-        widget=TextInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    email = EmailField(label='Email',
-        widget=EmailInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password1 = CharField(label='Password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password2 = CharField(label='Confirm password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
+class SignUpForm(auth_forms.UserCreationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={"class": "form-control", "required": True}),
+    )
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"class": "form-control", "required": True}),
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"class": "form-control", "required": True}),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(attrs={"class": "form-control", "required": True}),
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ("username", "email", "password1", "password2")
 
     def clean_username(self):
-        username = self.cleaned_data.get('username')
+        username = self.cleaned_data.get("username")
         qs = User.objects.filter(username=username)
         if qs.exists():
             raise ValidationError("Username already in use!")
@@ -52,8 +41,8 @@ class SignUpForm(UserCreationForm):
 
     def save(self, commit=True):
         instance = super().save(commit=False)
-        instance.first_name = self.cleaned_data.get('username', '').capitalize()
-        instance.email = self.cleaned_data.get('email', '')
+        instance.first_name = self.cleaned_data.get("username", "").capitalize()
+        instance.email = self.cleaned_data.get("email", "")
         if commit:
             instance.save()
         return instance
